488a6ccc4629b036522f6f5f90b439ab5ea87187
TheMax2 <max.malyshev.282@gmail.com>
2020-02-27 18:17:32 -0800

    added menu with working play btn

--- src/menu.js
+++ src/menu.js
@@ -1,1 +1,38 @@
 console.log("connected to the menu");
+var menu = document.getElementById("menu");
+var canvas = document.getElementById("gameWorld");
+var hints = document.getElementById("msg");
+var playBtn = document.getElementById("playBtn");
+var settBtn = document.getElementById("settBtn");
+var infoBtn = document.getElementById("infoBtn");
+var exitBtn = document.getElementById("exitBtn");
+
+hideGame = function(){
+    canvas.style.display = "none";
+    hints.style.display = "none";
+}
+hideGame();
+
+hideMenu = function() {
+    menu.style.display = "none";
+}
+
+showGame = function(){
+    canvas.style.display = "inline-block";
+    hints.style.display = "block";
+}
+
+showMenu = function(){
+    menu.style.display = "inline-block"
+}
+
+playGame = function(){
+    console.log("pressed play button");
+    hideMenu();
+    showGame();
+}
+
+playBtn.addEventListener('click', playGame);
+playBtn.addEventListener('mouseover', function(){
+    console.log("mouse is over");
+})
