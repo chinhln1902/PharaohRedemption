7d425e33aa8fcddbd9f3bcafab0c1444bf50db2f
TheMax2 <max.malyshev.282@gmail.com>
2020-03-05 13:08:40 -0800

    Added level editor and started lvl 2

---
+++ levels/Levels.js
@@ -0,0 +1,34 @@
+/* Welcome to the level editor. To create a level simply name it and 
+   start adding characters to an array of strings. 
+   Key: 
+        'space' : air
+        'C' : chisled bricks
+        'b' : bricks
+        'B' : brick top
+        's' : sand
+        'S' : sand top
+        '_' : spikes
+        'F' : floor - devides the over and under world platforms
+        'r' : rocks
+        'R' : rocks top
+
+*/
+var Level2Txt = [
+    "                                              ",
+    "                                              ",
+    "                                              ",
+    "               CCCC          C               B",
+    "         C                   C     B    B  B  ",
+    "                         B   b BBB    B       ",
+    "               S     S   b                    ",
+    "             SSsSS   s   b __     ____________",
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+    "                                              ",
+    "                                              ",
+    "                                              ",
+    "                      RRRR   B                ",
+    "         CBCC            b   C                ",
+    "                         b   b BB             ",
+    "               R     R   b       b            ",
+    "             RRrRR   r   b __     b           "
+]
