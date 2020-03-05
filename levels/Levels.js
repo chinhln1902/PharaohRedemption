89af1f30df0c22d6de2ee23e8261463d84fbe184
TheMax2 <max.malyshev.282@gmail.com>
2020-03-05 13:14:58 -0800

    documented levels class

--- levels/Levels.js
+++ levels/Levels.js
@@ -1,5 +1,8 @@
 /* Welcome to the level editor. To create a level simply name it and 
-   start adding characters to an array of strings. 
+   start adding characters to an array of strings. The levels can be 
+   as long as you want. Warning, make sure to not leave any spaces empty,
+   if you want, air, use the space, not the tab. the characters are case 
+   sensetive so be careful. Don't use any characters that aren't on the key.
    Key: 
         'space' : air
         'C' : chisled bricks
