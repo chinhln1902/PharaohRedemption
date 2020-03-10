267dadc048826ddf25fc8cf0e9bab5ed34a609f2
Gurkirat Jandu <44149587+gjandu3@users.noreply.github.com>
2020-03-10 15:36:14 -0700

    demon ai mostly done just need attacking and some underworld stuff

--- entities/Demon.js
+++ entities/Demon.js
@@ -13,11 +13,32 @@ function loadDemonSpriteSheets(AM) {
 
 }
 
-function Demon(game, AssetManager, startX, startY) {
+var underspritesheets = []; 
+function loadUnderSpriteSheets(AM) {
+	underspritesheets['idle'] = AM.getAsset("./assets/sprites/PurpleDemon/Idle/IdleSpriteSheet.png");
+	underspritesheets['idleflip'] = AM.getAsset("./assets/sprites/PurpleDemon/Idle/IdleSpriteSheetFlip.png");
+	underspritesheets['attack'] = AM.getAsset("./assets/sprites/PurpleDemon/Attacking/AttackSpriteSheet.png");
+	underspritesheets['attackflip'] = AM.getAsset("./assets/sprites/PurpleDemon/Attacking/AttackSpriteSheetFlip.png");
+	underspritesheets['dying'] = AM.getAsset("./assets/sprites/PurpleDemon/Dying/DyingSpriteSheet.png");
+	underspritesheets['dyingflip'] = AM.getAsset("./assets/sprites/PurpleDemon/Dying/DyingSpriteSheetFlip.png");
+	underspritesheets['walk'] = AM.getAsset("./assets/sprites/PurpleDemon/Walking/WalkingSpriteSheet.png");
+	underspritesheets['walkflip'] = AM.getAsset("./assets/sprites/PurpleDemon/Walking/WalkingSpriteSheetFlip.png");
+}
+
+function Demon(game, AssetManager, startX, startY, status) {
 	this.AM = AssetManager; 
-	loadDemonSpriteSheets(this.AM);
+	this.status = status
+	this.live = 1; 
+	this.underworld = false; 
+	if (this.status === 0) {
+		loadDemonSpriteSheets(this.AM);
+	} else {
+		loadUnderSpriteSheets(this.AM);
+		this.live = 0;
+		this.underworld = true; 
+	}
 	this.ctx = game.ctx; 
-	this.attack(); 
+	//this.idle(); 
 	this.state = "idle"; 
 	this.x = startX;
 	this.type = "enemy"; 
@@ -29,13 +50,15 @@ function Demon(game, AssetManager, startX, startY) {
 	this.pharaoh = game.mainCharacter; 
 	this.Left = false;
 	this.Right = true; 
+	this.walking = false; 
+	this.walkingright = false;
 	this.health = 3; 
 	this.immune = false;
+	this.play = 0; 
+	this.idleplay = 0; 
 	this.timeimmune = 0; 
 	this.name = "demon"; 
-	this.live = 1; 
 	this.PlayingTempAnimation = false; 
-	var underworld = false; 
 	var that = this; 
 	document.addEventListener("keyup", function (e) {
 		if (e.code === "Space"){
@@ -55,7 +78,7 @@ Demon.prototype = new Entity();
 Demon.prototype.constructor = Demon;
 
 Demon.prototype.draw = function () {
-	if (this.underworld) return; 
+	if (this.live === 0) return; 
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x - this.game.getCamera().getX(), this.y);
     Entity.prototype.draw.call(this);
 }
@@ -73,9 +96,27 @@ Demon.prototype.update = function () {
     this.x += this.game.clockTick * this.speed;
     ControlAnimation(this); 
     Entity.prototype.update.call(this);
-    /*if (this.game.getMainCharacter().getX() >= this.x - 175) {
-    	this.attack(); 
-    }*/
+    var mainX = this.game.getMainCharacter().getX(); 
+    if (this.walking) this.x -= 2; 
+    if (this.walkingright) this.x += 2; 
+    if (mainX >= this.x - 300 && mainX <= this.x - 150 && this.play === 0) {
+    	this.walk(); 
+    	this.idleplay = 0;
+    	this.walkingright = false; 
+    	this.play = 1; 
+    } 
+    if (mainX >= this.x + 150 && mainX <= this.x + 300 && this.play === 1) {
+    	this.walkright();
+    	this.idleplay = 0; 
+    	this.walking = false; 
+    	this.play = 0; 
+    }
+    if ((mainX < this.x - 300 && this.idleplay === 0) || (mainX > this.x + 300 && this.idleplay === 0)) {
+    	this.idleplay = 1; 
+    	this.walking = false;
+    	this.walkingright = false; 
+    	this.idle(); 
+    }
     for (var i = 0; i < this.game.entities.length; i++) {
     	var ent = this.game.entities[i];
     	if (ent.name === 'comet') {
@@ -109,7 +150,11 @@ Demon.prototype.hurtright = function() {
 }
 
 Demon.prototype.attack = function() {
-		this.animation = new Animation(demonspritesheets['attackflip'], 720, 480, 12, .05, 12, true, .5); 
+	if (this.status === 0) {
+		this.animation = new Animation(demonspritesheets['attackflip'], 720, 485, 12, .05, 12, false, .5); 
+	} else {
+		this.animation = new Animation(underspritesheets['attackflip'], 720, 485, 12, .05, 12, false, .5); 
+	}
 }
 
 Demon.prototype.attackright = function() {
@@ -117,16 +162,27 @@ Demon.prototype.attackright = function() {
 }
 
 Demon.prototype.walk = function() {
-	this.animation = new Animation(demonspritesheets['walk'], 720, 500, 18, .05, 18, true, .5); 
+	if (this.status === 0) {
+		this.animation = new Animation(demonspritesheets['walkflip'], 720, 500, 18, .05, 18, true, .5); 
+		this.walking = true;
+	} else {
+		this.animation = new Animation(underspritesheets['walkflip'], 720, 500, 18, .05, 18, true, .5); 
+		this.walking = true; 
+	}
 }
 
 Demon.prototype.walkright = function() {
-	this.animation = new Animation(demonspritesheets['walk'], 722, 480, 18, .05, 18, false, .5); 
+	this.animation = new Animation(demonspritesheets['walk'], 720, 500, 18, .05, 18, true, .5); 
+	this.walkingright = true; 
 }
 
 Demon.prototype.die = function() {
 	this.dead = true; 
-	this.animation = new Animation(demonspritesheets['dyingflip'], 722, 480, 15, .05, 15, false, .5);
+	if (this.status === 0) {
+		this.animation = new Animation(demonspritesheets['dyingflip'], 722, 480, 15, .05, 15, false, .5);
+	} else {
+		this.animation = new Animation(underspritesheets['dyingflip'], 722, 480, 15, .05, 15, false, .5); 
+	}
 }
 
 Demon.prototype.dieright = function() {
@@ -135,7 +191,12 @@ Demon.prototype.dieright = function() {
 }
 
 Demon.prototype.idle = function() {
-	this.animation = new Animation(demonspritesheets['idleflip'], 720, 450, 12, .08, 12, true, .5); 
+	//if (this.play === 0) return;  
+	if (this.status === 0) {
+		this.animation = new Animation(demonspritesheets['idleflip'], 720, 450, 12, .08, 12, true, .5); 
+	} else {
+		this.animation = new Animation(underspritesheets['idleflip'], 720, 450, 12, .08, 12, true, .5); 
+	}
 }
 
 Demon.prototype.idleright = function() {
